0ea8f50fd496f1532d3500c0e5303448c0f7f81c
Eric Brisco <erisco@Erics-MacBook-Pro.local>
2015-03-17 13:53:52 -0400

    ADD: extension AST to base AST in example01

--- example01/main.hs
+++ example01/main.hs
@@ -2,6 +2,10 @@ module Main where
 
 data Fix f = Fix (f (Fix f))
 
+--
+-- Base AST template
+--
+
 data Expression binOp expr =
     ExprInt   { exprInt :: Int
               }
@@ -12,6 +16,32 @@ data Expression binOp expr =
 --
 
 data BinOp = Add | Sub
+
+
+--
+-- Extending with more binary operations
+--
+
+data BinOpExt = Mul | Div | BinOpBase BinOp
+
+
+--
+-- Extending with unary operations
+--
+
+data ExpressionExt uniOp binOp expr =
+    ExprUniOp { exprUniOp :: uniOp
+              , exprArg   :: expr
+              }
+  | ExprBase (Expression binOp expr)
+--
+
+data UniOp = Fac | Log
+
+
+--
+-- Instantiation of the base AST.
+--
 
 type SimpleExpr = Fix (Expression BinOp)
 
@@ -27,6 +57,36 @@ evalSimpleExpr (Fix (ExprBinOp l op r)) = evalL `evalOp` evalR
 
 simple01 :: SimpleExpr
 simple01 = Fix (ExprBinOp (Fix (ExprInt 5)) Add (Fix (ExprInt 5)))
+
+
+--
+-- Instantiation of the extended AST.
+--
+
+type ExtendedExpr = Fix (ExpressionExt UniOp BinOpExt)
+
+evalExtendedExpr :: ExtendedExpr -> Int
+evalExtendedExpr (Fix (ExprUniOp op arg)) = evalOp evalArg
+  where evalOp = case op of
+                   Fac -> product . flip take [1..]
+                   Log -> floor . log . fromIntegral
+        evalArg = evalExtendedExpr arg
+evalExtendedExpr (Fix (ExprBase (ExprInt i))) = i
+evalExtendedExpr (Fix (ExprBase (ExprBinOp l op r))) = evalL `evalOp` evalR
+  where evalL = evalExtendedExpr l
+        evalR = evalExtendedExpr r
+        evalOp = case op of
+                   Mul -> (*)
+                   Div -> div
+                   BinOpBase Add -> (+)
+                   BinOpBase Sub -> (-)
+--
+
+extended01 :: ExtendedExpr
+extended01 = Fix (ExprUniOp Fac (Fix (ExprBase (ExprBinOp (Fix (ExprBase (ExprInt 3))) Mul (Fix (ExprBase (ExprInt 2)))))))
+
+
+-- This is to make the compiler happy.
 
 main :: IO ()
 main = putStrLn "Hello World"
